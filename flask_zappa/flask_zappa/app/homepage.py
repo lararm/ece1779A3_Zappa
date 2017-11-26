d562b8dc75d98ccbf74c5cb20a602fe675b30c37
Larissa <lrmadeir@gmail.com>
2017-11-26 01:36:36 -0500

    added search query

--- flask_zappa/flask_zappa/app/homepage.py
+++ flask_zappa/flask_zappa/app/homepage.py
@@ -10,8 +10,10 @@ import time
 import random
 import re
 from wand.image import Image
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from app import collage
+
+
 
 ALLOWED_IMAGE_EXTENSIONS = set(['image/png', 'image/jpg', 'image/jpeg', 'image/gif'])
 
@@ -27,54 +29,56 @@ def homepage():
 def upload_image_submit():
     print("#image_submit")
     #collage_test()
+    draw_retangle()
 
-    listofimages = ['flower1.jpg', 'flower2.jpg', 'flower3.jpg', 'flower4.jpg', 'flower5.jpg']
-    collage.make_collage(listofimages, 'collage5.jpg',500,450)
-    # Get Session Information
-    username = 'test' #escape(session['username'])
-
-    # Get User Input
-    image = request.files['image']
-    image_name = image.filename
-    image_type = image.content_type
-
-    # If user does not select file, browser also
-    # submit a empty part without filename
-    if image_name == '':
-        flash("No image selected for upload.")
-        return redirect(url_for('homepage'))
-
-    # Check image file extension
-    if not valid_image_extension(image_type):
-        flash("%s is not a valid image type. Must be of type [png,gif,jpeg,jpg]." % (image_type))
-        return redirect(url_for('homepage'))
-
-    # Create an S3 client
-    s3 = boto3.client('s3')
+    # listofimages = ['flower1.jpg', 'flower2.jpg', 'flower3.jpg', 'flower4.jpg', 'flower5.jpg']
+    # collage.make_collage(listofimages, 'collage5.jpg',500,450)
+    # # Get Session Information
+    # username = 'test' #escape(session['username'])
+    #
+    # # Get User Input
+    # image = request.files['image']
+    # image_name = image.filename
+    # image_type = image.content_type
+    #
+    # # If user does not select file, browser also
+    # # submit a empty part without filename
+    # if image_name == '':
+    #     flash("No image selected for upload.")
+    #     return redirect(url_for('homepage'))
+    #
+    # # Check image file extension
+    # if not valid_image_extension(image_type):
+    #     flash("%s is not a valid image type. Must be of type [png,gif,jpeg,jpg]." % (image_type))
+    #     return redirect(url_for('homepage'))
+    #
+    # # Create an S3 client
     # s3 = boto3.client('s3')
-    id = config.AWS_ID
-
-    # Creating unique name
-    timestamp = str(int(time.time()))
-    randomnum = str(random.randint(0, 10000))
-    unique_name = timestamp + "_" + randomnum + "_" + image_name
-
-    # Upload image to S3
-    image_new_name = username + "/" + unique_name
-    s3.upload_fileobj(image,
-                      id,
-                      image_new_name,
-                      ExtraArgs={"Metadata": {"Content-Type": image_type}})
-    image_url = (s3.generate_presigned_url('get_object', Params={'Bucket': id, 'Key': image_new_name},
-                                           ExpiresIn=100)).split('?')[0]
-
-    print("image url: " + image_url )
+    # # s3 = boto3.client('s3')
+    # id = config.AWS_ID
+    #
+    # # Creating unique name
+    # timestamp = str(int(time.time()))
+    # randomnum = str(random.randint(0, 10000))
+    # unique_name = timestamp + "_" + randomnum + "_" + image_name
+    #
+    # # Upload image to S3
+    # image_new_name = username + "/" + unique_name
+    # s3.upload_fileobj(image,
+    #                   id,
+    #                   image_new_name,
+    #                   ExtraArgs={"Metadata": {"Content-Type": image_type}})
+    # image_url = (s3.generate_presigned_url('get_object', Params={'Bucket': id, 'Key': image_new_name},
+    #                                        ExpiresIn=100)).split('?')[0]
+    #
+    # print("image url: " + image_url )
 
     return redirect(url_for('homepage'))
 
+
 @webapp.route('/query_submit', methods=['POST'])
 def query_submit():
-
+    print("#query_submit")
     tags = request.form['query']
     print (tags)
     #image_list = query_tag_table(tags)
@@ -107,5 +111,16 @@ def lambda_handler(event, context):
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
 
+def draw_retangle():
+    # "BoundingBox": {
+    #     "Width": 0.13733333349227905,
+    #     "Height": 0.20956256985664368,
+    #     "Left": 0.31066668033599854,
+    #     "Top": 0.15157680213451385
+    print("#draw retangle")
+    source_img = Image.open('flower1.jpg')
+    draw = ImageDraw.Draw(source_img)
+    draw.rectangle(((50, 50), (100, 100)),outline = "blue")
+    source_img.save("retangle.jpg")
 
 
