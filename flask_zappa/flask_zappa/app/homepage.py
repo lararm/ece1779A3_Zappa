8d09ecbd68e03fa8e1cbb98b7d794924e5f2e327
irfan-khan <irfana.khan@mail.utoronto.ca>
2017-12-02 20:43:59 -0500

    Fixed homepage lint errors

--- flask_zappa/flask_zappa/app/homepage.py
+++ flask_zappa/flask_zappa/app/homepage.py
@@ -224,25 +224,3 @@ def valid_image_extension(ext):
             return True
 
     return False
-<<<<<<< HEAD
-=======
-
-def lambda_handler(event, context):
-    print("#lambda_handler")
-    #print("Received event: " + json.dumps(event, indent=2))
-
-    # Get the object from the event and show its content type
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-        print("CONTENT TYPE: " + response['ContentType'])
-        print("Key: " + key)
-        return response['ContentType']
-    except Exception as e:
-        print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        raise e
-
-
->>>>>>> f131f222eb29fe5b13f199bbb6c3e729f6de940c
