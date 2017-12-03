2974f7cb629161a51de2ca23267b9355a5ef8076
Larissa <lrmadeir@gmail.com>
2017-12-02 20:15:53 -0500

    Changed login page and added sign up

--- flask_zappa/flask_zappa/app/homepage.py
+++ flask_zappa/flask_zappa/app/homepage.py
@@ -229,3 +229,5 @@ def lambda_handler(event, context):
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
+
+
